25d3d72b2c0ec43e2d6fd6f337f06039d7424666
arthursean <ascc@ic.ufal.br>
2023-07-28 13:04:58 -0300

    Inclusão de funcionalidades básicas do jogo e mudança do design do mapa

--- jogo.c
+++ jogo.c
@@ -9,13 +9,52 @@ void abrirMapa(char mapa[][24])
         fscanf(arquivo, "%s", mapa[i]);
     }
     fclose(arquivo);
+}
+void imprimirMapa(char mapa[][24])
+{
     for (int i = 0; i < 11; i++)
     {
         printf("%s\n", mapa[i]);
     }
 }
+int ehPosicaoValida(char mapa[][24], int l, int c)
+{
+    return mapa[l][c] == 'O';
+}
+void posicionamentoDasPecas(char mapa[][24], int vez)
+{
+    printf("Agora, vocês irão se alternar para posicionar suas peças nos pontos do tabuleiro\n");
+    int l, c;
+    scanf("%d %d", &l, &c);
+    while (!ehPosicaoValida(mapa, l, c))
+    {
+        printf("Posicao inválida, por favor, escolha uma posicao válida\n");
+        scanf("%d %d", &l, &c);
+    }
+    system("clear");
+    mapa[l][c] = vez % 2 == 0 ? 'X' : 'Y';
+    imprimirMapa(mapa);
+}
 int main()
 {
+    printf("Bem vindo(a) ao jogo do Moinho, para começar, escolha sua peça\n");
     char mapa[11][24];
     abrirMapa(mapa);
+    int escolhaDeJogador = -1;
+    while (escolhaDeJogador != 0 && escolhaDeJogador != 1)
+    {
+        system("clear");
+        printf("0 - 'X'\n1 - 'Y'\n");
+        printf("Obs: o 'X' começa primeiro\n");
+        scanf("%d", &escolhaDeJogador);
+    }
+    system("clear");
+    int vez = 0;
+    while (vez < 18)
+    {
+        posicionamentoDasPecas(mapa, vez);
+        vez += 1;
+    }
+    system("clear");
+    imprimirMapa(mapa);
 }
